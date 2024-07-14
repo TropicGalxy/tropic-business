5b890fbda67c035543f567531230e697c2ee6b66
TropicGalxy <104592582+TropicGalxy@users.noreply.github.com>
2024-07-14 01:40:29 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,34 +1,35 @@
-return {
-    Target = "ox", -- Set to either "qb" or "ox"
+Config = {}
 
-    businesses = {
-        burgershot = {
-            model = "u_m_y_burgerdrug_01", -- Set your ped model here
-            coords = vector4(-1187.4, -877.69, 12.83, 34.2), -- Set your ped coordinates here
-            job = "burgershot",
-            price = 32000,
-            blip = {
-                name = 'Burgershot',
-                sprite = 317,
-                color = 29,
-                scale = 0.6
-            },
-        },
+Config.Target = "ox" -- Set to either "qb" or "ox"
+
+Config.Businesses = {
+    {
+        EnableBlip = true,
+        BusinessName = "Burgershot",
+        BusinessPrice = 32000,
+        BusinessJob = "burgershot",
+        BusinessGrade = 4,
+        PedCoords = vector4(-1187.4, -877.69, 12.83, 34.2), -- Set your ped coordinates here
+        PedModel = "u_m_y_burgerdrug_01", -- Set your ped model here
+        BlipCoords = vector3(-1189.14, -890.5, 13.89), -- Set your blip coordinates here
+        BlipSprite = 106, -- Set your blip sprite here
+        BlipColor = 1, -- Set your blip color here
+        BlipName = "Burgershot", -- Set your blip name here
+        SellBackPercentage = 75
     },
-    business2 = {
-            model = "u_m_y_burgerdrug_01", -- Set your ped model here
-            coords = vector4(0, 0, 0, 0), -- Set your ped coordinates here
-            job = "job",
-            price = 0,
-            blip = {
-                name = 'My Business',
-                sprite = 317,
-                color = 29,
-                scale = 0.6
-            },
-        },
-    },
-
--- add more as needed
-
+    {
+        EnableBlip = false,
+        BusinessName = "My Business 2",
+        BusinessPrice = 75000,
+        BusinessJob = "businessowner2",
+        BusinessGrade = 4,
+        PedCoords = vector4(0.0, 0.0, 0.0, 0.0), -- Set your ped coordinates here
+        PedModel = "a_f_y_business_02", -- Set your ped model here
+        BlipCoords = vector3(0.0, 0.0, 0.0), -- Set your blip coordinates here
+        BlipSprite = 475, -- Set your blip sprite here
+        BlipColor = 2, -- Set your blip color here
+        BlipName = "Business 2", -- Set your blip name here
+        SellBackPercentage = 40
+    }
+    -- Add more businesses as needed
 }
